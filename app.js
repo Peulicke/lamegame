f56016e25efb0c359e4b67d3ecf6a55f62f5a6a0
Peulicke <oscarrpeulicke@gmail.com>
2018-07-10 17:50:36 +0200

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 
 app.get('/', function(req, res) {
-   res.sendfile('index.html');
+   res.sendfile('/index.html', { root:__dirname });
 });
 
 io.on('connection', function(socket) {
