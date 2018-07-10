6d05ceea96f04f05d4b051f0947967547b13d9c3
Peulicke <oscarrpeulicke@gmail.com>
2018-07-10 20:47:02 +0200

    test

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ var app = require('express')();
 var server = require('http').createServer(app);  
 var io = require('socket.io').listen(server);
 
-app.get('/', function(req, res) {
-   res.sendfile('/index.html', { root:__dirname });
+app.get('/', function(req, res){
+    res.sendfile('/index.html', { root:__dirname });
 });
 
-io.on('connection', function(socket) {
-   socket.on('clientEvent', function(data) {
-      console.log(data);
-      socket.emit('test', "Her er beskeden: " + data);
-   });
+server.listen(process.env.PORT || 4004, function(){
+    console.log('listening');
 });
 
-server.listen(process.env.PORT || 4004, function() {
-   console.log('listening');
+io.on('connection', function(socket){
+    socket.on('clientEvent', function(data){
+        console.log(data);
+        socket.emit('test', "Her er beskeden: " + data);
+    });
+    socket.on('disconnect', function(){
+        io.sockets.emit('user disconnected');
+    });
 });
